789cd16f931e280279ff59b008ad39847b300737
gyk0814 <49557580+gyk0814@users.noreply.github.com>
2023-09-14 16:25:26 +0900

    ADD: getting list of members

--- api/routes/groupRouter.js
+++ api/routes/groupRouter.js
@@ -3,5 +3,6 @@ const groupController = require("../controllers/groupController");
 const groupRouter = express.Router();
 
 groupRouter.post("/invitation", groupController.sendInvitation);
+groupRouter.get("/member", groupController.getMemberList);
 
 module.exports = groupRouter;
