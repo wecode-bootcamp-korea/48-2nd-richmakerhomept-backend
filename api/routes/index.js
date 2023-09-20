16ec2823f59a75678a21643290a9f20657f452ec
김만규 <123226237+KIMMANGYU2023@users.noreply.github.com>
2023-09-20 19:00:48 +0900

    Revert "firstPush-Jinhan"
    This reverts commit fa805a6f5c5f53a7f83e069b111767fd566e7ebc.

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,13 +3,9 @@ const groupRouter = require("./groupRouter");
 const route = express.Router();
 const userRoutes = require("./userRoutes");
 const providerRoutes = require("./providerRoutes");
-const financeDataRouter = require("./financeDataRouter");
-const transactionDataRouter = require("./transactionDataRouter");
 
 route.use("/user", userRoutes);
 route.use("/group", groupRouter);
 route.use("/providers", providerRoutes);
-route.use("/reports", financeDataRouter)
-route.use("/transactions", transactionDataRouter)
 
 module.exports = route;
--- api/services/index.js
+++ api/services/index.js
@@ -1,7 +1,4 @@
 const userServices = require("./userService");
 const providerServices = require("./providerService");
-const financeDataService = require("./financeDataService");
-const transactionDataService = require("./transactionDataService");
 
-module.exports = { userServices, providerServices,
-                financeDataService, transactionDataService };
+module.exports = { userServices, providerServices };
