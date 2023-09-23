66861751243a3dbebd3dbeb80c3faacc7ce20b96
kmjmarine <kmjmarine@naver.com>
2023-09-23 14:38:19 +0900

    [DELETE] files

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,8 +1,5 @@
 const mainControllers = require("./mainController");
 const userControllers = require("./userControllers");
 const providerControllers = require("./providerController");
-const financeDataController = require("./financeDataController");
-const transactionDataController = require("./transactionDataController");
 
 module.exports = { mainControllers, userControllers, providerControllers };
-
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,14 +4,10 @@ const mainRoutes = require("./mainRoutes");
 const groupRouter = require("./groupRouter");
 const userRoutes = require("./userRoutes");
 const providerRoutes = require("./providerRoutes");
-const financeDataRouter = require("./financeDataRouter");
-const transactionDataRouter = require("./transactionDataRouter");
 
 route.use("/main", mainRoutes);
 route.use("/user", userRoutes);
 route.use("/group", groupRouter);
 route.use("/providers", providerRoutes);
-route.use("/reports", financeDataRouter)
-route.use("/transactions", transactionDataRouter)
 
 module.exports = route;
